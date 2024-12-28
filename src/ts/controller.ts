e8155dc445914c5a2bae9b4112cf2ea73f6cc46b
Mario <mario.prskalo5@gmail.com>
2024-12-28 22:34:30 +0100

    rendering a recipe to html -- will finish alone later

--- src/ts/controller.ts
+++ src/ts/controller.ts
@@ -1,8 +1,28 @@
-interface Ingredients {
+interface ApiResponse {
+  status: string;
+  message: string;
+  data: {
+    recipe: ApiRecipe;
+  };
+}
+
+interface ApiRecipe {
+  id: string;
+  title: string;
+  publisher: string;
+  source_url: string;
+  image_url: string;
+  servings: number;
+  cooking_time: number;
+  ingredients: Ingredient[];
+}
+
+interface Ingredient {
   quantity: number;
   unit: string;
   description: string;
 }
+
 interface Recipe {
   id: string;
   title: string;
@@ -11,11 +31,11 @@ interface Recipe {
   image: string;
   servings: number;
   cookingTime: number;
-  ingredients: Ingredients;
+  ingredients: Ingredient[];
 }
 
 const recipeContainer = document.querySelector('.recipe');
-// https://forkify-api.jonas.io/
+
 if (!recipeContainer) {
   throw new Error('Recipe container not found');
 }
@@ -37,33 +57,137 @@ const timeout = function (s: number): Promise<never> {
   });
 };
 
-///////////////////////////////////////
-const ShowRecipe = async function (): Promise<void> {
+const showRecipe = async function (): Promise<void> {
   try {
+    // 1) Loading Recipe
     const res = await fetch(
       'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
-    const data = await res.json();
+    const data: ApiResponse = await res.json();
 
     if (!res.ok) {
       throw new Error(`${data.message} (${res.status})`);
     }
 
-    console.log(res, data);
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
+    // const apiRecipe = data.data.recipe;
+    // Destructuring with new name
+    const { recipe: apiRecipe } = data.data;
+
+    const recipe: Recipe = {
+      id: apiRecipe.id,
+      title: apiRecipe.title,
+      publisher: apiRecipe.publisher,
+      sourceUrl: apiRecipe.source_url,
+      image: apiRecipe.image_url,
+      servings: apiRecipe.servings,
+      cookingTime: apiRecipe.cooking_time,
+      ingredients: apiRecipe.ingredients,
     };
-  } catch (error) {
-    console.error(error);
+
+    console.log(recipe);
+
+    // 2) Rendering recipe
+    const markup = `<figure class="recipe__fig">
+          <img src="${recipe.image}" alt="${recipe.title}</" class="recipe__img" />
+          <h1 class="recipe__title">
+            <span>${recipe.title}</span>
+          </h1>
+        </figure>
+
+        <div class="recipe__details">
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="src/img/icons.svg#icon-clock"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+            <span class="recipe__info-text">minutes</span>
+          </div>
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="src/img/icons.svg#icon-users"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+            <span class="recipe__info-text">servings</span>
+
+            <div class="recipe__info-buttons">
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="src/img/icons.svg#icon-minus-circle"></use>
+                </svg>
+              </button>
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="src/img/icons.svg#icon-plus-circle"></use>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div class="recipe__user-generated">
+            <svg>
+              <use href="src/img/icons.svg#icon-user"></use>
+            </svg>
+          </div>
+          <button class="btn--round">
+            <svg class="">
+              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+            </svg>
+          </button>
+        </div>
+
+        <div class="recipe__ingredients">
+          <h2 class="heading--2">Recipe ingredients</h2>
+          <ul class="recipe__ingredient-list">
+            <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="src/img/icons.svg#icon-check"></use>
+              </svg>
+              <div class="recipe__quantity">1000</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">g</span>
+                pasta
+              </div>
+            </li>
+
+            <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="src/img/icons.svg#icon-check"></use>
+              </svg>
+              <div class="recipe__quantity">0.5</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">cup</span>
+                ricotta cheese
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        <div class="recipe__directions">
+          <h2 class="heading--2">How to cook it</h2>
+          <p class="recipe__directions-text">
+            This recipe was carefully designed and tested by
+            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+            directions at their website.
+          </p>
+          <a
+            class="btn--small recipe__btn"
+            href="${recipe.sourceUrl}"
+            target="_blank"
+          >
+            <span>Directions</span>
+            <svg class="search__icon">
+              <use href="src/img/icons.svg#icon-arrow-right"></use>
+            </svg>
+          </a>
+        </div>`;
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    } else {
+      console.error('An unknown error occurred');
+    }
   }
 };
 
-ShowRecipe();
+showRecipe();
