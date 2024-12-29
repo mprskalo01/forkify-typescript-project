29e6bd330a59b78e44862ddb576e9352e0a664f9
Mario <mario.prskalo5@gmail.com>
2024-12-29 17:41:53 +0100

    including ingredients and loading recipes on hash change

--- src/ts/controller.ts
+++ src/ts/controller.ts
@@ -1,3 +1,5 @@
+import icons from 'url:../img/icons.svg';
+
 interface ApiResponse {
   status: string;
   message: string;
@@ -57,11 +59,28 @@ const timeout = function (s: number): Promise<never> {
   });
 };
 
+const renderSpinner = function (parentEl: Element): void {
+  const markup = `
+        <div class="spinner">
+          <svg>
+            <use href="${icons}#icon-loader"></use>
+          </svg>
+        </div> 
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function (): Promise<void> {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
     // 1) Loading Recipe
+    renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
     );
     const data: ApiResponse = await res.json();
 
@@ -84,11 +103,11 @@ const showRecipe = async function (): Promise<void> {
       ingredients: apiRecipe.ingredients,
     };
 
-    console.log(recipe);
-
     // 2) Rendering recipe
     const markup = `<figure class="recipe__fig">
-          <img src="${recipe.image}" alt="${recipe.title}</" class="recipe__img" />
+          <img src="${recipe.image}" alt="${
+      recipe.title
+    }</" class="recipe__img" />
           <h1 class="recipe__title">
             <span>${recipe.title}</span>
           </h1>
@@ -97,27 +116,31 @@ const showRecipe = async function (): Promise<void> {
         <div class="recipe__details">
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-clock"></use>
+              <use href="${icons}#icon-clock"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+            <span class="recipe__info-data recipe__info-data--minutes">${
+              recipe.cookingTime
+            }</span>
             <span class="recipe__info-text">minutes</span>
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-users"></use>
+              <use href="${icons}#icon-users"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+            <span class="recipe__info-data recipe__info-data--people">${
+              recipe.servings
+            }</span>
             <span class="recipe__info-text">servings</span>
 
             <div class="recipe__info-buttons">
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="src/img/icons.svg#icon-minus-circle"></use>
+                  <use href="${icons}#icon-minus-circle"></use>
                 </svg>
               </button>
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="src/img/icons.svg#icon-plus-circle"></use>
+                  <use href="${icons}#icon-plus-circle"></use>
                 </svg>
               </button>
             </div>
@@ -125,12 +148,12 @@ const showRecipe = async function (): Promise<void> {
 
           <div class="recipe__user-generated">
             <svg>
-              <use href="src/img/icons.svg#icon-user"></use>
+              <use href="${icons}#icon-user"></use>
             </svg>
           </div>
           <button class="btn--round">
             <svg class="">
-              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+              <use href="${icons}#icon-bookmark-fill"></use>
             </svg>
           </button>
         </div>
@@ -138,9 +161,25 @@ const showRecipe = async function (): Promise<void> {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
+          ${recipe.ingredients
+            .map((ingredient: Ingredient): string => {
+              return `<li class="recipe__ingredient">
+                  <svg class="recipe__icon">
+                    <use href="${icons}#icon-check"></use>
+                  </svg>
+                  <div class="recipe__quantity">${
+                    ingredient.quantity || ''
+                  }</div>
+                  <div class="recipe__description">
+                    <span class="recipe__unit">${ingredient.unit}</span>
+                    ${ingredient.description}
+                  </div>
+                </li>`;
+            })
+            .join('')}
             <li class="recipe__ingredient">
               <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
+                <use href="${icons}#icon-check"></use>
               </svg>
               <div class="recipe__quantity">1000</div>
               <div class="recipe__description">
@@ -151,7 +190,7 @@ const showRecipe = async function (): Promise<void> {
 
             <li class="recipe__ingredient">
               <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
+                <use href="${icons}#icon-check"></use>
               </svg>
               <div class="recipe__quantity">0.5</div>
               <div class="recipe__description">
@@ -166,7 +205,9 @@ const showRecipe = async function (): Promise<void> {
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+            <span class="recipe__publisher">${
+              recipe.publisher
+            }</span>. Please check out
             directions at their website.
           </p>
           <a
@@ -176,12 +217,15 @@ const showRecipe = async function (): Promise<void> {
           >
             <span>Directions</span>
             <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
+              <use href="${icons}#icon-arrow-right"></use>
             </svg>
           </a>
         </div>`;
+
+    recipeContainer.innerHTML = '';
+
     recipeContainer.insertAdjacentHTML('afterbegin', markup);
-  } catch (err) {
+  } catch (err: unknown) {
     if (err instanceof Error) {
       console.error(err.message);
     } else {
@@ -190,4 +234,8 @@ const showRecipe = async function (): Promise<void> {
   }
 };
 
-showRecipe();
+// Listening for hash change
+
+['hashchange', 'load'].forEach((ev): void =>
+  window.addEventListener(ev, showRecipe)
+);
