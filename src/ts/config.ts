5373b2263b080671993cfa89223e9ec692f0164f
Mario <mario.prskalo5@gmail.com>
2025-01-10 16:39:57 +0100

    adding config.ts for netlify

---
+++ src/ts/config.ts
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+
+export const API_KEY = 'fd1da89b-3292-4a2d-bdda-3cb233acaa38';
+
+export const TIMEOUT_SECONDS = 10;
+
+export const RESULTS_PER_PAGE = 10;
+
+export const MODAL_CLOSE_SEC = 1.5
